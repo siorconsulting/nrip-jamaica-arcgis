9c8bb3df0721a9275a6966cfcf02c7383c98a7a6
Joe Lydon <77614346+JoeLydon@users.noreply.github.com>
2021-12-09 15:17:53 +0000

    Add files

---
+++ inundation_analysis.py
@@ -0,0 +1,42 @@
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+from conditional_eval import *
+
+__all__ = ['inundation_extents']
+
+def inundation_extents(ElevationRaster, list_thresholds=[0,5,10,15], out_raster_root_path=None, out_polygon_root_path=None):
+    """ Creates a set of inundation extents based on elevation thresholds and exports them as polygons and/or rasters' 
+  
+    Inputs:
+        ElevationRaster L raster
+        list_thresholds [optional] : list
+        out_raster_tool_path [optional] : str
+    
+    Returns:
+        None
+    """
+    
+    for i, th in enumerate(list_thresholds):
+        if i==0:
+
+            outRaster=set_null_above(ElevationRaster,th)
+            if out_raster_root_path is not None:
+                outRaster.save(f"{out_raster_root_path}_below_{str(th).replace('.','p')}")
+            if out_polygon_root_path is not None:
+                out_polygon_features = f"{out_polygon_root_path}_below_{str(th).replace('.','p')}"
+                arcpy.conversion.RasterToPolygon(outRaster, out_polygon_features)
+        if i<len(list_thresholds)-1:
+
+            low_th = th
+            high_th = list_thresholds[i+1]
+            outRaster = set_null_between(ElevationRaster, low_th, high_th)
+            if out_raster_root_path is not None:
+                outRaster.save(f"{out_raster_root_path}_from_{str(low_th).replace('.','p')}_to_{str(high_th).replace('.','p')}")
+            if out_polygon_root_path is not None:
+                out_polygon_features = f"{out_polygon_root_path}_from_{str(low_th).replace('.','p')}_to_{str(high_th).replace('.','p')}"
+                arcpy.conversion.RasterToPolygon(outRaster, out_polygon_features)
+   
+
+
+
